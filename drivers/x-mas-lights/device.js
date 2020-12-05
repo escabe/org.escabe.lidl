c38814316181074f63852ff8c793ac4512d1da14
EscAbe <martijn.aben@gmail.com>
2020-12-05 23:54:18 +0100

    Added color dim.

--- drivers/x-mas-lights/device.js
+++ drivers/x-mas-lights/device.js
@@ -7,6 +7,7 @@ debug(true);
 
 class MyZigBeeDevice extends ZigBeeDevice {
     transactionID = 0;
+    zclNode;
 
     make4String(v) {
         let s = Math.round(v).toString(16);
@@ -17,51 +18,88 @@ class MyZigBeeDevice extends ZigBeeDevice {
         else return '0000';
     }
 
+    setColor({dim,light_hue,light_saturation}) {
+        if (dim===undefined) dim = this.getCapabilityValue('dim');
+        if (light_hue==undefined) light_hue = this.getCapabilityValue('light_hue');
+        if (light_saturation==undefined) light_saturation = this.getCapabilityValue('light_saturation');
+        this.transactionID++;
+        this.transactionID %= 256;
+        this.zclNode.endpoints[1].clusters.tuya.setString({
+            status: 0,
+            transid: this.transactionID,
+            dp: 5,
+            datatype: 3,
+            length: 0,
+            data: this.make4String(light_hue * 360) + this.make4String(light_saturation * 1000) + this.make4String(dim * 1000)
+        });
+    }
+
+    setWhiteDim({dim}) {
+        if (dim===undefined) dim = this.getCapabilityValue('dim');
+        this.transactionID++;
+        this.transactionID %= 256;
+        this.zclNode.endpoints[1].clusters.tuya.setUInt32({
+            status: 0,
+            transid: this.transactionID,
+            dp: 3,
+            datatype: 2,
+            length: 4,
+            data: dim * 1000
+        });
+    }
+
     async onNodeInit({ zclNode }) {
+        this.zclNode = zclNode;
+        this.registerCapabilityListener('onoff', async (value) => {
+            this.transactionID++;
+            this.transactionID %= 256;
+            zclNode.endpoints[1].clusters.tuya.setBool({
+                status: 0,
+                transid: this.transactionID,
+                dp: 1,
+                datatype: 1,
+                length: 1,
+                data: value
+            });
+        });
 
-        this.registerCapability('onoff', CLUSTER.ON_OFF);
-
-        this.registerCapabilityListener('lidl_xmas_mode', async (value) => {
-            this.log("Setting to " + value);
+        this.registerCapabilityListener('lidl_xmas_mode', async (mode) => {
             const valMap = {white: 0, color: 1, scene: 2};
             this.transactionID++;
             this.transactionID %= 256;
-            zclNode.endpoints[1].clusters.tuya.setEnum({
+            await zclNode.endpoints[1].clusters.tuya.setEnum({
                 status: 0,
                 transid: this.transactionID,
                 dp: 2,
                 datatype: 4,
                 length: 1,
-                data: valMap[value]
+                data: valMap[mode]
             });
+            switch (mode) {
+                case 'color':
+                    this.setColor({});
+                    break;
+                case 'white':
+                default:
+                    this.setWhiteDim({});
+            }
         });
 
-        this.registerCapabilityListener('dim', async (value) => {
-            this.log("Setting to " + value);
-            const valMap = {white: 0, color: 1, scene: 2};
-            this.transactionID++;
-            this.transactionID %= 256;
-            zclNode.endpoints[1].clusters.tuya.setUInt32({
-                status: 0,
-                transid: this.transactionID,
-                dp: 3,
-                datatype: 2,
-                length: 4,
-                data: value * 1000
-            });
+        this.registerCapabilityListener('dim', async (dim) => {
+            const mode = this.getCapabilityValue('lidl_xmas_mode');
+            switch (mode) {
+                case 'color':
+                    this.setColor({dim});
+                    break;
+                case 'white':
+                default:
+                    this.setWhiteDim({dim});
+            }
         });
+
         this.registerMultipleCapabilityListener(['light_hue','light_saturation'],(values,options) => {
-            this.log(values);
-            this.transactionID++;
-            this.transactionID %= 256;
-            zclNode.endpoints[1].clusters.tuya.setString({
-                status: 0,
-                transid: this.transactionID,
-                dp: 5,
-                datatype: 3,
-                length: 0,
-                data: this.make4String(values.light_hue * 360) + this.make4String(values.light_saturation * 1000) + this.make4String(500)
-            });
+            this.setCapabilityValue('lidl_xmas_mode','color');
+            this.setColor(values);
         },500);
 
 
