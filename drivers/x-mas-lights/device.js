c096287a8d12c8d02ac78affc6470479e09cf7b0
EscAbe <martijn.aben@gmail.com>
2020-12-05 14:43:18 +0100

    On/off, dim and mode implemented.

--- drivers/x-mas-lights/device.js
+++ drivers/x-mas-lights/device.js
@@ -7,21 +7,22 @@ const { debug, CLUSTER, Cluster } = require('zigbee-clusters');
 debug(true);
 
 class MyZigBeeDevice extends ZigBeeDevice {
+    transactionID = 0;
 
     async onNodeInit({ zclNode }) {
-
         this.registerCapability('onoff', CLUSTER.ON_OFF);
 
-        this.registerCapabilityListener('my_mode', async (value) => {
+        this.registerCapabilityListener('lidl_xmas_mode', async (value) => {
             console.log("Setting to " + value);
             const valMap = {white:0,color:1,scene:2};
+            this.transactionID++;
+            this.transactionID %= 256;
             zclNode.endpoints[1].clusters.tuya.setEnum({
                 status: 0,
                 transid:  0,
                 dp: 2,
                 datatype: 4,
-                length_hi: 0,
-                length_lo: 1,
+                length: 1,
                 data: valMap[value]
               });
         });
@@ -29,18 +30,19 @@ class MyZigBeeDevice extends ZigBeeDevice {
         this.registerCapabilityListener('dim', async (value) => {
           console.log("Setting to " + value);
           const valMap = {white:0,color:1,scene:2};
+          this.transactionID++;
+          this.transactionID %= 256;
           zclNode.endpoints[1].clusters.tuya.setUInt32({
               status: 0,
               transid:  0,
               dp: 3,
               datatype: 2,
-              length_hi: 0,
-              length_lo: 4,
+              length: 4,
               data: value * 1000
             });
       });
 
-      }
+  }
 
 }
 
