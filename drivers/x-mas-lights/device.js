65b0fce6aa6b1ac57c835da186433640f64746af
EscAbe <martijn.aben@gmail.com>
2020-12-06 12:49:58 +0100

    Refactored to work with one datapoint functions

--- drivers/x-mas-lights/device.js
+++ drivers/x-mas-lights/device.js
@@ -6,9 +6,67 @@ const { debug, CLUSTER, Cluster } = require('zigbee-clusters');
 debug(true);
 
 class MyZigBeeDevice extends ZigBeeDevice {
-    transactionID = 0;
     zclNode;
 
+    async onNodeInit({ zclNode }) {
+        this.zclNode = zclNode;
+
+        // Handler for on/off
+        this.registerCapabilityListener('onoff', async (value) => {
+            return this.writeBool(1, value);
+        });
+
+        // Handler for switching modes
+        this.registerCapabilityListener('lidl_xmas_mode', async (mode) => {
+            // Actually switch modes
+            const valMap = {white: 0, color: 1, scene: 2};
+            await this.writeEnum(2,valMap[mode]);
+            // Then call methods to also set last chosen dim level or color
+            switch (mode) {
+                case 'color':
+                    return this.setColor({});
+                case 'white':
+                default:
+                    return this.setWhiteDim({});
+            }
+        });
+
+        // Handler for dim
+        this.registerCapabilityListener('dim', async (dim) => {
+            // Check the current mode
+            const mode = this.getCapabilityValue('lidl_xmas_mode');
+            switch (mode) {
+                case 'color': // In color mode set color again but with new dim level
+                    return this.setColor({dim});
+                case 'white': // In white mode simply directly set dim
+                default:
+                    return this.setWhiteDim({dim});
+            }
+        });
+
+        // Handler for color
+        this.registerMultipleCapabilityListener(['light_hue','light_saturation'],(values,options) => {
+            // When choosing a color, actually switch to color mode
+            this.setCapabilityValue('lidl_xmas_mode','color');
+            // Then set the color based on newly selected value
+            return this.setColor(values);
+        },500);
+    }
+
+    async setColor({dim,light_hue,light_saturation}) {
+        if (dim===undefined) dim = this.getCapabilityValue('dim');
+        if (light_hue===undefined) light_hue = this.getCapabilityValue('light_hue');
+        if (light_saturation===undefined) light_saturation = this.getCapabilityValue('light_saturation');
+        return this.writeString(5,this.make4String(light_hue * 360) + this.make4String(light_saturation * 1000) + this.make4String(dim * 1000))
+
+    }
+
+    async setWhiteDim({dim}) {
+        if (dim===undefined) dim = this.getCapabilityValue('dim');
+        return this.writeData32(3,dim*1000);
+    }
+
+    //region String Helper Functions
     make4String(v) {
         let s = Math.round(v).toString(16);
         if(s.length===4) return s;
@@ -17,93 +75,68 @@ class MyZigBeeDevice extends ZigBeeDevice {
         else if(s.length===1) return '000' + s;
         else return '0000';
     }
+    //endregion
 
-    setColor({dim,light_hue,light_saturation}) {
-        if (dim===undefined) dim = this.getCapabilityValue('dim');
-        if (light_hue==undefined) light_hue = this.getCapabilityValue('light_hue');
-        if (light_saturation==undefined) light_saturation = this.getCapabilityValue('light_saturation');
-        this.transactionID++;
-        this.transactionID %= 256;
-        this.zclNode.endpoints[1].clusters.tuya.setString({
+    //region Tuya Datapoint Functions
+    _transactionID = 0;
+    set transactionID(val) {
+        this._transactionID = val % 256;
+    }
+    get transactionID() {
+        return this._transactionID;
+    }
+
+    async writeBool(dp, value) {
+        const data = Buffer.alloc(1);
+        data.writeUInt8(value ? 0x01 : 0x00,0);
+        return this.zclNode.endpoints[1].clusters.tuya.datapoint({
             status: 0,
-            transid: this.transactionID,
-            dp: 5,
-            datatype: 3,
-            length: 0,
-            data: this.make4String(light_hue * 360) + this.make4String(light_saturation * 1000) + this.make4String(dim * 1000)
+            transid: this.transactionID++,
+            dp,
+            datatype: 1,
+            length: 1,
+            data
         });
     }
 
-    setWhiteDim({dim}) {
-        if (dim===undefined) dim = this.getCapabilityValue('dim');
-        this.transactionID++;
-        this.transactionID %= 256;
-        this.zclNode.endpoints[1].clusters.tuya.setUInt32({
+    async writeEnum(dp, value) {
+        const data = Buffer.alloc(1);
+        data.writeUInt8(value, 0);
+        return this.zclNode.endpoints[1].clusters.tuya.datapoint({
             status: 0,
-            transid: this.transactionID,
-            dp: 3,
+            transid: this.transactionID++,
+            dp,
+            datatype: 4,
+            length: 1,
+            data
+        });
+    }
+
+    async writeData32 (dp, value) {
+        const data = Buffer.alloc(4);
+        data.writeUInt32BE(value,0);
+        return this.zclNode.endpoints[1].clusters.tuya.datapoint({
+            status: 0,
+            transid: this.transactionID++,
+            dp,
             datatype: 2,
             length: 4,
-            data: dim * 1000
+            data
         });
     }
 
-    async onNodeInit({ zclNode }) {
-        this.zclNode = zclNode;
-        this.registerCapabilityListener('onoff', async (value) => {
-            this.transactionID++;
-            this.transactionID %= 256;
-            zclNode.endpoints[1].clusters.tuya.setBool({
-                status: 0,
-                transid: this.transactionID,
-                dp: 1,
-                datatype: 1,
-                length: 1,
-                data: value
-            });
+    async writeString(dp, value) {
+        const data = Buffer.from(String(value),'latin1');
+        return this.zclNode.endpoints[1].clusters.tuya.datapoint({
+            status: 0,
+            transid: this.transactionID++,
+            dp,
+            datatype: 3,
+            length: value.length,
+            data
         });
-
-        this.registerCapabilityListener('lidl_xmas_mode', async (mode) => {
-            const valMap = {white: 0, color: 1, scene: 2};
-            this.transactionID++;
-            this.transactionID %= 256;
-            await zclNode.endpoints[1].clusters.tuya.setEnum({
-                status: 0,
-                transid: this.transactionID,
-                dp: 2,
-                datatype: 4,
-                length: 1,
-                data: valMap[mode]
-            });
-            switch (mode) {
-                case 'color':
-                    this.setColor({});
-                    break;
-                case 'white':
-                default:
-                    this.setWhiteDim({});
-            }
-        });
-
-        this.registerCapabilityListener('dim', async (dim) => {
-            const mode = this.getCapabilityValue('lidl_xmas_mode');
-            switch (mode) {
-                case 'color':
-                    this.setColor({dim});
-                    break;
-                case 'white':
-                default:
-                    this.setWhiteDim({dim});
-            }
-        });
-
-        this.registerMultipleCapabilityListener(['light_hue','light_saturation'],(values,options) => {
-            this.setCapabilityValue('lidl_xmas_mode','color');
-            this.setColor(values);
-        },500);
-
-
-  }
+    }
+    //endregion
 
 }
 
